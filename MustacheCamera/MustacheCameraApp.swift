1479c1a4d40e5a7b2b8fec637b9937b1033dfe37
ParsannaK <prasanna.koirala302@gmail.com>
2024-11-23 22:00:28 -0500

    Initial Commit

---
+++ MustacheCamera/MustacheCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MustacheCameraApp.swift
+//  MustacheCamera
+//
+//  Created by Prasanna Koirala on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MustacheCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MustacheCameraTests/MustacheCameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  MustacheCameraTests.swift
+//  MustacheCameraTests
+//
+//  Created by Prasanna Koirala on 11/23/24.
+//
+
+import Testing
+@testable import MustacheCamera
+
+struct MustacheCameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
